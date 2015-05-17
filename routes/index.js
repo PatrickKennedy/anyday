f8adcde304aa8d3b9b1955413d50527987e5a0d1
Patrick Kennedy <pakattack161@gmail.com>
2015-05-17 15:43:34 -0600

    cos: update index.jade to use jade style directive declarations

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*
+ * GET index
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
