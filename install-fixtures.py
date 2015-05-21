30117d3ba68586f0faf2634ecf3ef6a15ec331fa
Patrick Kennedy <pakattack161@gmail.com>
2015-05-20 23:01:53 -0600

    add: fixture installation settings and ability to delete docs not present in fixtures file

--- install-fixtures.py
+++ install-fixtures.py
@@ -9,14 +9,28 @@ with open('./config.json', 'r') as cf:
 with open('./fixtures.json', 'r') as cf:
     fixtures = json.load(cf)
 
-r.connect(config['rethinkdb']['host'], config['rethinkdb']['port']).repl()
+r.connect(
+    config['rethinkdb']['host'],
+    config['rethinkdb']['port'],
+    config['rethinkdb']['db'],
+).repl()
 
 for (table, fixture) in fixtures.items():
+    config = fixture.pop(0)
+    ids = []
     for item in fixture:
-        obj = r.db('anyday').table(table).get(item['id']).run()
-        op = r.db('anyday').table(table)
+        if item['id'] == "config":
+            continue
+        ids.append(item['id'])
+        obj = r.table(table).get(item['id']).run()
+        op = r.table(table)
         if obj:
             op = op.get(item['id']).update(item)
         else:
             op = op.insert(item)
         print op.run()
+
+    if config.get('delete_missing', False):
+        print r.table(table).filter(
+            lambda doc: ~r.expr(ids).contains(doc['id'])
+        ).delete().run()
