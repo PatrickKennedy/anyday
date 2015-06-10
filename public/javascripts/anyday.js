2f237e6f5df8e14d101e0ebde55ac68025b1ad9b
Patrick Kennedy <pakattack161@gmail.com>
2015-06-10 01:33:02 -0600

    ref: separate related pieces into their own angular modules

--- public/javascripts/anyday.js
+++ public/javascripts/anyday.js
@@ -1,8 +1,106 @@
 (function(angular){
-    var app = angular.module('anyday', []);
+    /*
+     * AnyConfig module
+     * Contains server-side configuration variables
+     */
+    angular.module('any.config', [])
+        .provider('AnyConfig', function () {
+            var config = Object.extend({
+                  user: null,
+            });
+            return {
+                  set: function (diff) {
+                      config = config.merge(diff);
+                  },
+                  $get: function () {
+                      return config;
+                  }
+            };
+        });
 
-    app
-        .controller('AnydayController', ['$scope', 'AnyAPI', function($scope, api) {
+    /*
+     * AnyApi module
+     * Contains all logic related to the api functionality
+     */
+    angular.module('any.api', [])
+        .factory('AnyAPI', [
+            '$http',
+            function ($http) {
+            var prefix = '/api/v1';
+            return {
+              fixtures: function () {
+                var url = prefix+'/task-fixtures/';
+                return $http.get(url);
+              },
+              tasks: function () {
+                var url = prefix+'/tasks/';
+                return $http.get(url);
+              },
+              create: function (task) {
+                var url = prefix+'/tasks/';
+                return $http.post(url, task);
+              },
+              update: function (task) {
+                var url = prefix+'/tasks/' + task.id;
+                return $http.put(url, task);
+              },
+              delete: function(id) {
+                var url = prefix+'/tasks/' + id;
+                return $http.delete(url);
+              },
+              sendtoken: function(email) {
+                var url = prefix+'/sendtoken/';
+                return $http.post(url, {email: email});
+              }
+            };
+          }
+        ])
+    ;
+
+    /*
+     * AnyLogin module
+     * Contains all logic related to the login functionality
+     */
+    angular.module('any.login', ['any.api', 'anyday.templates', 'login.jade'])
+        .controller('AnydayLoginFormController', [
+            '$scope', 'AnyAPI',
+            function($scope, api) {
+                $scope.enter_token = false;
+                $scope.reset_token = function (){ $scope.enter_token = false; }
+                $scope.submit_login = function (){
+                    console.log('attempting to send token');
+                    if ($scope.email) {
+                        api.sendtoken($scope.email)
+                        .success(function(result){
+                            console.log('token sent successfully');
+                            $scope.enter_token = true;
+                        })
+                        .error(function(error){
+                            console.log('token failed to send: '+ error);
+                        })
+                        ;
+                    }
+                }
+            }
+        ])
+        .directive('anyLoginForm', [
+            function () {
+                return {
+                    templateUrl: 'login.jade',
+                }
+            }
+        ])
+    ;
+
+
+    /*
+     * Anyday module
+     * Contains core website logic
+     */
+    angular.module('anyday', ['any.api', 'any.login', 'anyday.templates', 'login.jade'])
+        .controller('AnydayController', [
+            '$scope', 'AnyAPI',
+            function($scope, api) {
             //$scope.tasks = [
             //    {
             //        name: "Shower",
@@ -56,21 +154,7 @@
                 });
             }
         }])
-        .controller('AnydayLoginFormController', ['$scope', 'AnyAPI', function($scope, api) {
-            $scope.submit_login = function (){
-                console.log('attempting to send token');
-                if ($scope.email) {
-                    api.sendtoken($scope.email)
-                    .success(function(result){
-                        console.log('token sent successfully');
-                    })
-                    .error(function(error){
-                        console.log('token failed to send: '+ error);
-                    })
-                    ;
-                }
-            }
-        }])
+        
         .directive('anyFixture', [
             function () {
                 return {
@@ -96,35 +180,6 @@
                 }
             }
         ])
-        .factory('AnyAPI', function ($http) {
-            var prefix = '/api/v1';
-            return {
-              fixtures: function () {
-                var url = prefix+'/task-fixtures/';
-                return $http.get(url);
-              },
-              tasks: function () {
-                var url = prefix+'/tasks/';
-                return $http.get(url);
-              },
-              create: function (task) {
-                var url = prefix+'/tasks/';
-                return $http.post(url, task);
-              },
-              update: function (task) {
-                var url = prefix+'/tasks/' + task.id;
-                return $http.put(url, task);
-              },
-              delete: function(id) {
-                var url = prefix+'/tasks/' + id;
-                return $http.delete(url);
-              },
-              sendtoken: function(email) {
-                var url = prefix+'/sendtoken/';
-                return $http.post(url, {email: email});
-              }
-            };
-          })
-        ;
+    ;
 }(angular));
 
