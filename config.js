0fa44886785f5a79f20b6147e2d0ebf9485b9ba4
Patrick Kennedy <pakattack161@gmail.com>
2015-08-05 01:25:19 -0600

    mod: update config to merge in local settings and be env aware

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-var config = require('./config.json')
+require('sugar');
 
-config
+var config = require('./config.json')
+    , local = require('./local.json')
+    ;
+
+config = Object.merge(config, local);
+
+config.rethinkdb.host = process.env.RETHINKDB_HOST || config.rethinkdb.host
+config.rethinkdb.port = process.env.RETHINKDB_PORT || config.rethinkdb.port
 
 module.exports = config;
